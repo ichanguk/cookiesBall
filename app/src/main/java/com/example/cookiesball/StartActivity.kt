7cfd89245e529817924438a1130da5285ffe5b4f
changukLee <ckddnr5527@gmail.com>
2024-04-28 21:04:31 +0900

    feat: Update activity_main

--- app/src/main/java/com/example/cookiesball/StartActivity.kt
+++ app/src/main/java/com/example/cookiesball/StartActivity.kt
@@ -1,12 +1,16 @@
 package com.example.cookiesball
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.cookiesball.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStartBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,13 @@ class StartActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
